384db79683f2ba674fe764d320beef2d9b66d8da
Cardinal <avror1g@yandex.ru>
2023-10-30 15:04:15 +0300

    lesson 2

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,11 @@
-console.log('file 1');
+const hamburger = document.querySelector('.hamburger'),
+	menu = document.querySelector('.menu'),
+	close = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+	menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+	menu.classList.remove('active');
+});
